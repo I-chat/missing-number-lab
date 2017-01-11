c527fdd38681d19778fd175dbb43cbb2d8cf90e7
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-11 03:12:08 +0100

    Fix bug

---
+++ app/m.js
@@ -0,0 +1,23 @@
+var findMissing = function (arr1, arr2) {
+	var match = false
+	if (arr1.length === 0 && arr2.length === 0) {
+		return 0
+	} else if (arr1.length > arr2.length) {
+		for (var i in arr1) {
+			for (var prop in arr2) {
+				if (arr1[i] === arr2[prop]) {
+					match = true;
+				}
+			}
+			}
+	} else if (arr1.length < arr2.length) {
+		for (var i in arr2) {
+			for (var prop in arr1) {
+				if (arr2[i] === arr1[prop]) {
+					match = true;
+				}
+			};
+		}
+	}
+	}
+}
