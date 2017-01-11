959b7bbea77d80b03861d2e9fb09188f1346a917
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-11 12:10:36 +0100

    Delete unrelated files

--- app/missing.js
+++ app/missing.js
@@ -1,24 +1,28 @@
-var findMissing = function (arr1, arr2) {
-  var match = false
-  if (arr1.length === arr2.length) {
-    return 0
-  } else if (arr1.length >= arr2.length) {
-    for (var i in arr1) {
-      for (var prop in arr2) {
-        if (arr1[i] === arr2[prop]) {
-          match = true;
-        }
-      }
-      }
-  } else if (arr1.length < arr2.length) {
-    for (var i in arr2) {
-      for (var prop in arr1) {
-        if (arr2[i] === arr1[prop]) {
-          match = true;
-        }
-      };
-    }
+var findMissing = function(arr1,arr2) {
+   
+    match = false,
+    found = 0;
+
+  if(arr1.length === arr2.length) {
+    return 0;
   }
-  };
+
+  for (var key in arr2) {
+   
+    for ( var prop in arr1) {
+      if (arr2[key] == arr1[prop]){
+        match = true;
+        break;
+      } else {
+        match = false;
+      }
+    }
+  if (match === false) {
+    found = arr2[key]
+  }
+  }
+ return found;
+};
+
 
 module.exports = findMissing
